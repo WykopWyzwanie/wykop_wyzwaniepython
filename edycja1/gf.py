be5e6bc1d5d8a433e556a2b364476051e321081b
Qof Naught <qof.naught@gmail.com>
2016-07-03 21:19:23 +0200

    Wypełnienie pliku losowymi bajtami, z generowaniem duplikatów

--- edycja1/gf.py
+++ edycja1/gf.py
@@ -3,11 +3,23 @@ import random
 import sys
 import tempfile
 import time
+import string
+
+
+RANDOM_DATA_LEN = 10
+
+def random_data():
+    return "".join(random.SystemRandom().choice(
+        string.ascii_lowercase + string.digits) for _ in range(RANDOM_DATA_LEN))
 
 if __name__ == '__main__':
     assert len(sys.argv) > 1
     k = int(sys.argv[1])
+    n = k // 3
+    rds = [random_data() for _ in range(n)]
     for _ in range(k):
         f = tempfile.NamedTemporaryFile(dir='.', delete=False)
+        with open(f.name, 'w') as tf:
+            tf.write(rds[random.randint(0, n - 1)])
         os.utime(f.name, (random.randint(0, int(time.time())),
-                          random.randint(0, int(time.time()))))
+                            random.randint(0, int(time.time()))))
