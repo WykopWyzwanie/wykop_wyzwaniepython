77d6f2b341134ea65e0790c20297d60e9541cb4d
Qof Naught <qof.naught@gmail.com>
2016-07-05 23:16:52 +0200

    Pierwsza wersja skryptu sprawdzającego

---
+++ edycja1/test_rozwiazania.py
@@ -0,0 +1,34 @@
+import os
+import os.path as path
+import sys
+import time
+
+def check_file(out_dir, in_dir, filepath):
+    ystr, mstr = time.strftime("%Y %m", time.gmtime(os.path.getmtime(filepath))).split(' ')
+    if out_dir == ystr and in_dir == mstr:
+        return True
+    else:
+        print("zle: {}, {}, {}".format(filepath, ystr, mstr))
+        return False
+
+
+def get_dirs_filter(path):
+    return filter(lambda x: os.path.isdir(os.path.join(path, x)), os.listdir(path))
+
+def check_solution(working_dir):
+    all_files = 0
+    good_moves = 0
+    lambda x, filepath: os.path.isdir()
+    for out_dir in get_dirs_filter(working_dir):
+        for in_dir in get_dirs_filter(os.path.join(working_dir, out_dir)):
+            for f in os.listdir(os.path.join(working_dir, out_dir, in_dir)):
+                if check_file(out_dir, in_dir,
+                        os.path.join(working_dir, out_dir, in_dir, f)):
+                    good_moves += 1
+                all_files += 1
+    print("{}/{} plikow dobrze skatalogowanych".format(good_moves, all_files))
+
+
+if __name__ == '__main__':
+    working_dir = sys.argv[1] if len(sys.argv) > 1 else "."
+    check_solution(os.path.abspath(working_dir))
