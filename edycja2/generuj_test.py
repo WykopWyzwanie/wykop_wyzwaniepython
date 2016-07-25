eb62690dabb5bb9dcd404b1f6ff4179d7a1cbf01
Qof Naught <qof.naught@gmail.com>
2016-07-25 21:56:20 +0200

    Dodanie losowego rozkładu występowania rozszerzeń

--- edycja2/generuj_test.py
+++ edycja2/generuj_test.py
@@ -13,22 +13,34 @@ EXTENSIONS = (
         )
 
 
+def cumsum(l):
+    total = 0
+    for x in l:
+        total += x
+        yield total
+
+
+DEVIATION = 50
+DISTRIBUTION = tuple(cumsum([
+    random.randint(1, DEVIATION) for _ in range(len(EXTENSIONS))]))
+MAX_INDEX = max(DISTRIBUTION)
+
+
 def random_ext():
-    return random.choice(EXTENSIONS)
+    roulette = random.randint(0, MAX_INDEX)
+    i = 0
+    while DISTRIBUTION[i] < roulette:
+        i += 1
+    return EXTENSIONS[i]
 
 
 def random_data(max_length=50):
     return os.urandom(random.randint(1, max_length))
 
 
-def random_dir():
-    pass
-
-
-def fill_directory(path, mind=3, maxd=12, minf=5, maxf=10, level=0):
+def fill_directory(path, mind=2, maxd=10, minf=8, maxf=16, level=0):
     nbr_of_dirs = random.randint(max(0, mind - level), max(0, maxd - 2 * level))
     nbr_of_files = random.randint(max(0, minf - level), max(0, maxf - 2 * level))
-    print('{}/{}'.format(nbr_of_dirs, nbr_of_files))
     for _ in range(nbr_of_files):
         with tempfile.NamedTemporaryFile(dir=path, suffix=random_ext(), delete=False) as f:
             f.write(random_data())
@@ -37,12 +49,11 @@ def fill_directory(path, mind=3, maxd=12, minf=5, maxf=10, level=0):
         fill_directory(os.path.join(path, dirname), level=level+1)
 
 
-
 def generate_test(main_dir_name):
     try:
         os.mkdir(main_dir_name)
     except OSError:
-        print('WARN: Katalog {} juz istnieje'.format(main_dir_name), file=sys.stderr)
+        sys.stderr.write('WARN: Katalog {} juz istnieje'.format(main_dir_name), file=sys.stderr)
     fill_directory(os.path.abspath(main_dir_name))
 
 
